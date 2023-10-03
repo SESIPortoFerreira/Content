b0227c299a8f033fe50e13896d49635be90a66d4
pedrohambrosioo <enzoggw015@gmail.com>
2023-10-03 20:51:01 -0300

    Javascript render
    SOS, eu vou m mata

---
+++ script.js
@@ -0,0 +1,111 @@
+var container = document.querySelector('.conteudobox1')
+container.style.display = 'none'
+
+var btnJogos = document.querySelector('#Jogos')
+
+btnJogos.addEventListener('click', function(){
+    var container = document.querySelector('.conteudobox1')
+    container.style.display = 'none'
+    var divJogos = document.querySelectorAll('.jogos')
+    divJogos.forEach(function(div){
+        div.style.display = 'flex'
+    })
+    //divJogos.style.display = 'flex'
+})
+
+var btnDiaadia = document.querySelector('#Diaadia')
+
+btnDiaadia.addEventListener('click', function(){
+    var container = document.querySelector('.conteudobox1')
+    container.style.display = 'none'
+    var divDiaadia = document.querySelectorAll('.diaadia')
+    divDiaadia.forEach(function(div){
+        div.style.display = 'flex'
+    })
+})
+
+var btnExposicoes = document.querySelector('#Exposicoes')
+
+btnExposicoes.addEventListener('click', function(){
+    var container = document.querySelector('.conteudobox1')
+    container.style.display = 'none'
+    var divExposicoes = document.querySelectorAll('.exposicoes')
+    divExposicoes.forEach(function(div){
+        div.style.display = 'flex'
+    })
+})
+
+var btnTrabalhos = document.querySelector('#Trabalhos')
+
+btnTrabalhos.addEventListener('click', function(){
+    var container = document.querySelector('.conteudobox1')
+    container.style.display = 'none'
+    var divTrabalhos = document.querySelectorAll('.trabalhos')
+    divTrabalhos.forEach(function(div){
+        div.style.display = 'flex'
+    })
+})
+
+var btnProjetos = document.querySelector('#Projetos')
+
+btnProjetos.addEventListener('click', function(){
+    var container = document.querySelector('.conteudobox1')
+    container.style.display = 'none'
+    var divProjetos = document.querySelectorAll('.projetos')
+    divProjetos.forEach(function(div){
+        div.style.display = 'flex'
+    })
+})
+
+var btnModa = document.querySelector('#Moda')
+
+btnModa.addEventListener('click', function(){
+    var container = document.querySelector('.conteudobox1')
+    container.style.display = 'none'
+    var divModa = document.querySelectorAll('.moda')
+    divModa.forEach(function(div){
+        div.style.display = 'flex'
+    })
+})
+
+var btnPoema = document.querySelector('#Poemas')
+
+btnPoema.addEventListener('click', function(){
+    var container = document.querySelector('.conteudobox1')
+    container.style.display = 'none'
+    var divPoema = document.querySelectorAll('.poemas')
+    divPoema.forEach(function(div){
+        div.style.display = 'flex'
+    })
+})
+
+var btnMusica = document.querySelector('#musica')
+
+btnMusica.addEventListener('click', function(){
+    var container = document.querySelector('.conteudobox1')
+    container.style.display = 'none'
+    var divMusica = document.querySelectorAll('.musica')
+    divMusica.forEach(function(div){
+        div.style.display = 'flex'
+    })
+})
+
+var btnLivros = document.querySelector('#livro')
+
+btnLivros.addEventListener('click', function(){
+    var container = document.querySelector('.conteudobox1')
+    container.style.display = 'none'
+    var divLivros = document.querySelectorAll('.livro')
+    divLivros.forEach(function(div){
+        div.style.display = 'flex'
+    })
+})
+
+
+
+    
+
+
+
+
+    
